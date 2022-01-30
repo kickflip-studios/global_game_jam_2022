3f3eb79a94eb1138ec7d614c5ee165d68c54cd0a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-01-30 17:55:22 +1100

    add collision fix

--- src/constants.rs
+++ src/constants.rs
@@ -4,6 +4,8 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
 };
 
+
+pub const SPRITE_DIR: &str = "assets";
 pub const PLAYER_SPRITE: &str = "player.png";
 pub const ELECTRON_SPRITE: &str = "electron.png";
 pub const POSITRON_SPRITE: &str = "positron.png";
@@ -13,15 +15,22 @@ pub const BACKGROUND_SPRITE: &str = "backgound.png";
 
 pub const WALL_THICKNESS: f32 = 10.;
 
-pub const SCALE: f32 = 0.1;
+pub const SCALE: f32 = 0.5;
 pub const TIME_STEP: f32 = 1. / 60.;
 
-pub const SCREEN_WIDTH: f32 = 600.0;
-pub const SCREEN_HEIGHT: f32 = 500.0;
+pub const SCREEN_WIDTH: f32 = 1000.0;
+pub const SCREEN_HEIGHT: f32 = 1000.0;
 
 #[derive(Component)]
 pub enum Collider {
     Wall,
     Particle,
 	Player,
+}
+
+
+// region:    Resources
+pub struct SpriteInfos {
+	pub player: (Handle<Image>, Vec2),
+	pub particle: (Handle<Image>, Vec2),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-#![allow(unused)]
 
+#![allow(unused)]
 
 use bevy::{
     core::FixedTimestep,
     prelude::*,
+	render::texture::ImageType,
     sprite::collide_aabb::{collide, Collision},
 };
-
+use std::path::Path;
 
 mod constants;
 mod player;
@@ -15,9 +16,8 @@ mod walls;
 
 use crate::constants::*;
 use player::{ PlayerPlugin};
-use particle::{ParticlePlugin};
+use particle::ParticlePlugin;
 use walls::spawn_walls;
-
 
 
 #[derive(Component)]
@@ -34,30 +34,32 @@ fn main() {
 			height:  constants::SCREEN_HEIGHT,
 			..Default::default()
 		})
-
-		.add_plugins(DefaultPlugins)
-		.add_plugin(ParticlePlugin)
-		.add_plugin(PlayerPlugin)
-		.add_startup_system(setup)
 		.add_startup_system(spawn_walls)
-		// .add_system_set(
-        //     SystemSet::new()
-        //         .with_run_criteria(FixedTimestep::step(constants::TIME_STEP as f64))
-		// 		.with_system(player_movement)
-		// 		.with_system(particle_collision_system)
-		// )
-
+		.add_plugins(DefaultPlugins)
+		.add_plugin(PlayerPlugin)
+		.add_plugin(ParticlePlugin)
+		.add_startup_system(setup)
 		.run();
 }
 
 
 
-
-
+fn load_image(images: &mut ResMut<Assets<Image>>, path: &str) -> (Handle<Image>, Vec2) {
+	// Note - With bevy v0.6, load images directly and synchronously to capture size
+	//        See https://github.com/bevyengine/bevy/pull/3696
+	let path = Path::new(SPRITE_DIR).join(path);
+	let bytes = std::fs::read(&path).expect(&format!("Cannot find {}", path.display()));
+	let image = Image::from_buffer(&bytes, ImageType::MimeType("image/png")).unwrap();
+	let size = image.texture_descriptor.size;
+	let size = Vec2::new(size.width as f32, size.height as f32);
+	let image_handle = images.add(image);
+	(image_handle, size)
+}
 
 fn setup(
 	mut commands: Commands,
 	asset_server: Res<AssetServer>,
+	mut images: ResMut<Assets<Image>>,
 	mut materials: ResMut<Assets<ColorMaterial>>,
 	mut windows: ResMut<Windows>,
 ){
@@ -67,4 +69,17 @@ fn setup(
 	// Position windows on your monitor
 	let mut window = windows.get_primary_mut().unwrap();
 	window.set_position(IVec2::new(1000, 0));
+
+
+
+	commands.insert_resource(SpriteInfos {
+		player: load_image(&mut images, PLAYER_SPRITE),
+		particle: load_image(&mut images, POSITRON_SPRITE),
+	});
+
+	let mut collider_query: Query<(Entity, &Transform, &Sprite, &Collider)>;
+
+	info!("Player sprite size: {:?}", load_image(&mut images, PLAYER_SPRITE).1);
+	info!("Particle sprite size: {:?}", load_image(&mut images, POSITRON_SPRITE));
+
 }
